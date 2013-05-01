8d87198e7e77d06626bcadd9e88ae2ca48482285
Brian Early <earlyb@vt.edu>
2013-05-01 10:57:43 -0400

    Added initial domain objects and common abstract classes and interfaces.
    AFDP-32

---
+++ org.ualerts.fixed/org.ualerts.fixed.service/src/main/java/org/ualerts/fixed/service/CommandService.java
@@ -0,0 +1,58 @@
+/*
+ * File created on May 1, 2013 
+ *
+ * Copyright 2008-2013 Virginia Polytechnic Institute and State University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.ualerts.fixed.service;
+
+/**
+ * A service interface for obtaining and invoking {@link Command} objects.
+ *
+ * @author Carl Harris
+ */
+public interface CommandService {
+
+  /**
+   * Creates a new command instance given the class of the command.  The command
+   * must have been annotated with {@link CommandComponent}.
+   * @param commandClass the class of the command
+   * @return a new instance of the command
+   * @throws Exception as needed
+   */
+  @SuppressWarnings("rawtypes")
+  <T extends Command> T newCommand(Class<T> commandClass) throws Exception;
+  
+  /**
+   * Creates a new command instance given the name and class of the command bean.  The command
+   * must have been annotated with {@link CommandComponent}.
+   * @param beanName the name of the bean
+   * @param commandClass the class of the command
+   * @return a new instance of the command
+   * @throws Exception as needed
+   */
+  @SuppressWarnings("rawtypes")
+  <T extends Command> T newCommand(String beanName, Class<T> commandClass)
+      throws Exception;
+
+  /**
+   * Invokes/executes the command.
+   * @param command the command to execute
+   * @return whatever object the command returns.
+   * @throws Exception as needed
+   */
+  <T> T invoke(Command<T> command) throws Exception;
+  
+}
---
+++ org.ualerts.fixed/org.ualerts.fixed.service/src/main/java/org/ualerts/fixed/service/commands/AbstractCommand.java
@@ -0,0 +1,60 @@
+/*
+ * File created on May 1, 2013 
+ *
+ * Copyright 2008-2013 Virginia Polytechnic Institute and State University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.ualerts.fixed.service.commands;
+
+import org.springframework.beans.factory.InitializingBean;
+
+import org.ualerts.fixed.service.Command;
+
+/**
+ * An abstract base for {@link Command} implementations.
+ *
+ * @author Carl Harris
+ */
+public abstract class AbstractCommand<T> 
+    implements Command<T>, InitializingBean {
+
+  /**
+   * @inheritDoc
+   */
+  public void afterPropertiesSet() throws Exception {
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public final T execute() throws Exception {
+    onValidate();
+    return onExecute();
+  }
+
+  /**
+   * Performs any required validation of command state and member parameters.
+   * @throws Exception if there is a validation error.
+   */
+  protected void onValidate() throws Exception {
+  }
+
+  /**
+   * Performs the execution of the command.
+   * @return a reference to the object the command uses, as appropriate.
+   * @throws Exception as needed.
+   */
+  protected abstract T onExecute() throws Exception;
+}
