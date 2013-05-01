1e62aa24b09a269bcc076d80220ddbc1e78fbbfa
Michael Irwin <mikesir87@gmail.com>
2013-05-01 13:58:38 -0400

    Created FixtureDTO
    IN PROGRESS - issue AFDP-31: Create UI for Manual Fixture Enrollment 
    https://vt-alerts.atlassian.net/browse/AFDP-31

---
+++ org.ualerts.fixed/org.ualerts.fixed.web/src/main/java/org/ualerts/fixed/web/dto/FixtureDTO.java
@@ -0,0 +1,134 @@
+/*
+ * File created on May 1, 2013 
+ *
+ * Copyright 2008-2013 Virginia Polytechnic Institute and State University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.ualerts.fixed.web.dto;
+
+/**
+ * A Data Transfer Object representing a fixture.
+ *
+ * @author Michael Irwin
+ */
+public class FixtureDTO {
+
+  private String building;
+  private String room;
+  private String positionHint;
+  private String inventoryNumber;
+  private String serialNumber;
+  private String ipAddress;
+  private String macAddress;
+  
+  /**
+   * Gets the {@code building} property.
+   */
+  public String getBuilding() {
+    return building;
+  }
+  
+  /**
+   * Sets the {@code building} property.
+   */
+  public void setBuilding(String building) {
+    this.building = building;
+  }
+  
+  /**
+   * Gets the {@code room} property.
+   */
+  public String getRoom() {
+    return room;
+  }
+  
+  /**
+   * Sets the {@code room} property.
+   */
+  public void setRoom(String room) {
+    this.room = room;
+  }
+  
+  /**
+   * Gets the {@code positionHint} property.
+   */
+  public String getPositionHint() {
+    return positionHint;
+  }
+  
+  /**
+   * Sets the {@code positionHint} property.
+   */
+  public void setPositionHint(String positionHint) {
+    this.positionHint = positionHint;
+  }
+  
+  /**
+   * Gets the {@code inventoryNumber} property.
+   */
+  public String getInventoryNumber() {
+    return inventoryNumber;
+  }
+  
+  /**
+   * Sets the {@code inventoryNumber} property.
+   */
+  public void setInventoryNumber(String inventoryNumber) {
+    this.inventoryNumber = inventoryNumber;
+  }
+  
+  /**
+   * Gets the {@code serialNumber} property.
+   */
+  public String getSerialNumber() {
+    return serialNumber;
+  }
+  
+  /**
+   * Sets the {@code serialNumber} property.
+   */
+  public void setSerialNumber(String serialNumber) {
+    this.serialNumber = serialNumber;
+  }
+  
+  /**
+   * Gets the {@code ipAddress} property.
+   */
+  public String getIpAddress() {
+    return ipAddress;
+  }
+  
+  /**
+   * Sets the {@code ipAddress} property.
+   */
+  public void setIpAddress(String ipAddress) {
+    this.ipAddress = ipAddress;
+  }
+  
+  /**
+   * Gets the {@code macAddress} property.
+   */
+  public String getMacAddress() {
+    return macAddress;
+  }
+  
+  /**
+   * Sets the {@code macAddress} property.
+   */
+  public void setMacAddress(String macAddress) {
+    this.macAddress = macAddress;
+  }
+  
+}
