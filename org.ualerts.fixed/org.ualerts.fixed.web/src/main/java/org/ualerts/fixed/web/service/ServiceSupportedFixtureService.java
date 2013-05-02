325213af3fee4032c21946cf8431879491ceebb4
Michael Irwin <mikesir87@gmail.com>
2013-05-02 18:07:13 -0400

    Created ServiceSupportedFixtureService and its test case
    AFDP-31

---
+++ org.ualerts.fixed/org.ualerts.fixed.web/src/main/java/org/ualerts/fixed/web/service/ServiceSupportedFixtureService.java
@@ -0,0 +1,72 @@
+/*
+ * File created on May 2, 2013
+ *
+ * Copyright 2008-2013 Virginia Polytechnic Institute and State University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.ualerts.fixed.web.service;
+
+import javax.annotation.Resource;
+
+import org.ualerts.fixed.Fixture;
+import org.ualerts.fixed.service.CommandService;
+import org.ualerts.fixed.service.commands.AddFixtureCommand;
+import org.ualerts.fixed.service.errors.ValidationErrorCollection;
+import org.ualerts.fixed.web.dto.FixtureDTO;
+
+/**
+ * An implementation of the {@link FixtureService} class that uses commands from
+ * the {@code org.ualerts.service} module to complete the various tasks.
+ * 
+ * @author Michael Irwin
+ */
+public class ServiceSupportedFixtureService implements FixtureService {
+
+  private CommandService commandService;
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void createFixture(FixtureDTO fixture)
+      throws ValidationErrorCollection, Exception {
+
+    AddFixtureCommand command =
+        commandService.newCommand(AddFixtureCommand.class);
+    command.setBuildingName(fixture.getBuilding());
+    command.setInetAddress(fixture.getIpAddressObj());
+    command.setInventoryNumber(fixture.getInventoryNumber());
+    command.setMacAddress(fixture.getMacAddressObj());
+    command.setPositionHint(fixture.getPositionHint());
+    command.setRoomNumber(fixture.getRoom());
+    command.setSerialNumber(fixture.getSerialNumber());
+    
+    Fixture newFixture = commandService.invoke(command);
+    
+    fixture.setId(newFixture.getId());
+    fixture.setVersion(newFixture.getVersion());
+  }
+
+  /**
+   * Sets the {@code commandService} property.
+   * @param commandService the value to set
+   */
+  @Resource
+  public void setCommandService(CommandService commandService) {
+    this.commandService = commandService;
+  }
+
+}
---
+++ org.ualerts.fixed/org.ualerts.fixed.web/src/test/java/org/ualerts/fixed/web/service/ServiceSupportedFixtureServiceTest.java
@@ -0,0 +1,89 @@
+/*
+ * File created on May 2, 2013
+ *
+ * Copyright 2008-2013 Virginia Polytechnic Institute and State University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.ualerts.fixed.web.service;
+
+import junit.framework.Assert;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.junit.Before;
+import org.junit.Test;
+import org.ualerts.fixed.Fixture;
+import org.ualerts.fixed.InetAddress;
+import org.ualerts.fixed.MacAddress;
+import org.ualerts.fixed.service.CommandService;
+import org.ualerts.fixed.service.commands.AddFixtureCommand;
+import org.ualerts.fixed.web.dto.FixtureDTO;
+
+/**
+ * Test case for the {@link ServiceSupportedFixtureService} class.
+ *
+ * @author Michael Irwin
+ */
+public class ServiceSupportedFixtureServiceTest {
+
+  private Mockery context;
+  private FixtureDTO fixture;
+  private AddFixtureCommand command;
+  private CommandService commandService;
+  private ServiceSupportedFixtureService service;
+  
+  @Before
+  public void setup() {
+    context = new Mockery();
+    command = new AddFixtureCommand();
+    commandService = context.mock(CommandService.class);
+    service = new ServiceSupportedFixtureService();
+    service.setCommandService(commandService);
+    
+    fixture = new FixtureDTO();
+    fixture.setBuilding("A Building");
+    fixture.setInventoryNumber("INV-234567890");
+    fixture.setIpAddress("192.168.1.1");
+    fixture.setIpAddressObj(InetAddress.getByAddress(fixture.getIpAddress()));
+    fixture.setMacAddress("0A:12:34:0B:56:78");
+    fixture.setMacAddressObj(new MacAddress(fixture.getMacAddress()));
+    fixture.setPositionHint("TOP-RIGHT");
+    fixture.setRoom("100");
+    fixture.setSerialNumber("0ABCDEF123456");
+  }
+  
+  @Test
+  public void validateFixtureCreation() throws Exception {
+    final Fixture fixtureObj = new Fixture();
+    fixtureObj.setId(123L);
+    fixtureObj.setVersion(456L);
+    
+    context.checking(new Expectations() {{
+      oneOf(commandService).newCommand(with(AddFixtureCommand.class));
+      will(returnValue(command));
+      oneOf(commandService).invoke(with(command));
+      will(returnValue(fixtureObj));
+    }});
+    
+    service.createFixture(fixture);
+    
+    context.assertIsSatisfied();
+    Assert.assertEquals(fixtureObj.getId(), fixture.getId());
+    Assert.assertEquals(fixtureObj.getVersion(), fixture.getVersion());
+  }
+  
+  
+}
