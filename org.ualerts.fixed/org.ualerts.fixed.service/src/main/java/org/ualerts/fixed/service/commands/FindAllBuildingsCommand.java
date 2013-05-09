1b93666b8fb56c4954d616d6f2bcb55be9ad73be
Brian Early <earlyb@vt.edu>
2013-05-09 09:55:58 -0400

    Added all buildings query support.
    AFDP-42
    
    Added/changed the following:
    
    - Added query for all buildings
    - Added repository function to use this query.
    - Added command to use this function.
    - Added unit and integration tests.

---
+++ org.ualerts.fixed/org.ualerts.fixed.service/src/main/java/org/ualerts/fixed/service/commands/FindAllBuildingsCommand.java
@@ -0,0 +1,62 @@
+/*
+ * File created on May 9, 2013 
+ *
+ * Copyright 2008-2013 Virginia Polytechnic Institute and State University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.ualerts.fixed.service.commands;
+
+import java.util.List;
+
+import javax.persistence.PersistenceException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.ualerts.fixed.Building;
+import org.ualerts.fixed.repository.BuildingRepository;
+import org.ualerts.fixed.service.CommandComponent;
+import org.ualerts.fixed.service.errors.UnspecifiedConstraintException;
+
+/**
+ * Command to find all buildings in the UAlerts system.
+ *
+ * @author Brian Early
+ */
+@CommandComponent
+public class FindAllBuildingsCommand extends AbstractCommand<List<Building>> {
+  private BuildingRepository repository;
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  protected List<Building> onExecute() throws UnspecifiedConstraintException {
+    try {
+      return repository.findAllBuildings();
+    }
+    catch (PersistenceException ex) {
+      throw new UnspecifiedConstraintException(ex);
+    }
+  }
+
+  /**
+   * Sets the {@code repository} property.
+   * @param repository the building repository
+   */
+  @Autowired
+  public void setRepository(BuildingRepository repository) {
+    this.repository = repository;
+  }
+
+}
