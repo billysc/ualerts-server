b0c97b7558a54eecef15339f52ad319c898c329a
Michael Irwin <mikesir87@gmail.com>
2013-05-13 19:33:00 -0400

    New fixture displays on fixture view and is highlighted briefly
    AFDP-65

--- org.ualerts.fixed/org.ualerts.fixed.web/src/main/webapp/assets/js/index.js
+++ org.ualerts.fixed/org.ualerts.fixed.web/src/main/webapp/assets/js/index.js
@@ -1,7 +1,9 @@
+var fixturesListTable;
+
 
 $(function() {
-  $("#fixturesList").dataTable({
-    aaSorting: [ [0, 'asc'], [1, 'asc'] ],
+  fixturesListTable = $("#fixturesList").dataTable({
+	aaSorting: [ [0, 'asc'], [1, 'asc'] ],
     bPaginate: false,
     bInfo: false,
     aoColumns: [
@@ -22,8 +24,39 @@ function postModalDisplay_enrollFixture($modal) {
     var requestType = "POST";
     var responseType = "json";
     var successCallback = function(data) {
-      if (data.success)
+      if (data.success) {
+    	var fixture = data.fixture;
+
+      $("#fixturesListEmpty").remove();
+
+    	//Add the row. Result has index value to retrieve row from dataTables
+    	var row = $("#fixturesList").show().dataTable().fnAddData([
+    	  fixture.buildingAbbreviation + " " + fixture.room,
+    	  fixture.positionHint,
+    	  fixture.ipAddress,
+    	  fixture.macAddress,
+    	  fixture.inventoryNumber,
+    	  "<a href='#'>Details</a>"
+    	]);
+    	
+    	var $newRow = $( fixturesListTable.fnGetNodes( row[0] ) );
+    	var currentColor = $newRow.css("backgroundColor");
+    	$newRow.addClass("updated");
+    	
+    	// Scope this section so variables can't be changed
+    	with ({$row: $newRow, originalColor : currentColor}) {
+    		setTimeout(function() {
+    			$row.find("td").animate({backgroundColor: originalColor}, 1000, 
+    		      function() {
+    				// Remove the class and clear out css attributes for bgColor
+    				$row.removeClass("updated").find("td")
+    				  .css("backgroundColor", "");
+    			  }
+    			);
+    		}, 4000);
+    	}
         $modal.modal('hide');
+      }
       else {
         displayErrorsOnForm($form, data.errors);
         $(".modal-body").scrollTop(0);
