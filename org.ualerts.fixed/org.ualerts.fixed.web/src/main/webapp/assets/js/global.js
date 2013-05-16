3cafebd9f605c4d729c984bd15ee35b8012bc80c
Michael Irwin <mikesir87@gmail.com>
2013-05-16 08:52:10 -0400

    Added loaded event to bootstrap modal and updated global.js to use it
    AFDP-41

--- org.ualerts.fixed/org.ualerts.fixed.web/src/main/webapp/assets/js/global.js
+++ org.ualerts.fixed/org.ualerts.fixed.web/src/main/webapp/assets/js/global.js
@@ -14,16 +14,17 @@ $(function() {
     var primaryButtonText = $this.data("primarybuttontext");
     var cancelButtonText = $this.data("cancelbuttontext");
     
-    var opts = {};
-    if (typeof remote != "undefined") {
-      opts.remote = remote;
-    }
-    
     // Remove any lingering click handlers
     var $primaryButton = $target.find(".btn-primary").unbind("click");
     var $cancelButton = $target.find(".btn.cancel").unbind("click");
     
+    var opts = {};
+    if (typeof remote != "undefined") {
+      opts.remote = remote;
+    }
+
     $target.modal(opts);
+    
     $target.find(".modal-header h3").text(title);
     if (typeof primaryButtonText == "string")
       $primaryButton.removeClass("hide").text(primaryButtonText);
@@ -35,15 +36,17 @@ $(function() {
     else
       $cancelButton.addClass("hide");
     
-    if (typeof $this.data("postmodalcallback") == "string") {
-      var fn = $this.data("postmodalcallback");
-      window[fn]($target);
-    }
-    
     // Destroy the modal when closed to force URL fetch
     $target.on('hidden', function() {
       $(this).removeData('modal');
     });
+    
+    if (typeof $this.data("postmodalcallback") == "string") {
+      var fn = $this.data("postmodalcallback");
+      if (typeof window[fn] == "function") {
+        $target.on('loaded', window[fn]($target));
+      }
+    }
     
     return false;
   });
