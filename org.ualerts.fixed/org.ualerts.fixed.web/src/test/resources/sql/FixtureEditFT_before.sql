718c42193126451f520446f0c1a6e4eb3362843e
Michael Irwin <mikesir87@gmail.com>
2013-05-21 15:38:34 -0400

    First stab at the functional test for fixture editing
    AFDP-48

---
+++ org.ualerts.fixed/org.ualerts.fixed.web/src/test/resources/sql/FixtureEditFT_before.sql
@@ -0,0 +1,20 @@
+INSERT INTO building(id, abbreviation, name)
+VALUES(building.1.id, 'building.1.abbreviation', 'building.1.name');
+
+INSERT INTO building(id, abbreviation, name)
+VALUES(building.2.id, 'building.2.abbreviation', 'building.2.name');
+
+INSERT INTO room(id, building_id, room_number)
+VALUES(room.1.id, building.1.id, 'room.1.roomNumber');
+
+INSERT INTO position_hint(id, hint_text)
+VALUES(positionHint.1.id, 'positionHint.1.hintText');
+
+INSERT INTO asset(id, serial_number, inventory_number, mac_address)
+VALUES(asset.1.id, 'asset.1.serialNumber', 'asset.1.inventoryNumber', 
+  'asset.1.macAddress');
+  
+INSERT INTO fixture(id, version, room_id, position_hint_id, asset_id, ip_address, 
+    installed_by)
+VALUES(fixture.1.id, fixture.1.version, fixture.1.roomId, fixture.1.positionHintId, 
+    fixture.1.assetId, 'fixture.1.ipAddress', 'fixture.1.installedBy');
