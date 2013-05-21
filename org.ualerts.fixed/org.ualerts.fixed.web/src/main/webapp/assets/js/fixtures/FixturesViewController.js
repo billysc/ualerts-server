44430b8c3ad14dc751c477736a5ff2c5a31e22e2
Michael Irwin <mikesir87@gmail.com>
2013-05-21 11:15:06 -0400

    Changed entity-id to be added as DOM attribute, rather than data attr
    AFDP-51

--- org.ualerts.fixed/org.ualerts.fixed.web/src/main/webapp/assets/js/fixtures/FixturesViewController.js
+++ org.ualerts.fixed/org.ualerts.fixed.web/src/main/webapp/assets/js/fixtures/FixturesViewController.js
@@ -83,7 +83,7 @@ FixturesViewController.prototype.addFixtureToView = function(fixture) {
   
   // Get new row and wrap as jQuery object
   var $newRow = $(this.fixturesListTable.fnGetNodes(row[0]));
-  $newRow.data("entity-id", fixture.id);
+  $newRow.attr("data-entity-id", fixture.id);
   var currentColor = $newRow.css("backgroundColor");
   $newRow.addClass("updated");
   
